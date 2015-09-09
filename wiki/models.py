dab089d841259a117b94cfbb4357d21c4bc14d14
sonubudwal <sonubudwal20@gmail.com>
2015-09-09 11:01:00 -0700

    Initial commit

---
+++ wiki/models.py
@@ -0,0 +1,6 @@
+from django.db import models
+
+# Create your models here.
+class Page(models.Model):
+    name = models.CharField(max_length="20",primary_key=True)
+    contect = models.TextField(blank=True)
